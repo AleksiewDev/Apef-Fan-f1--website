a09c991c1a2290b31ce6611a3b92f774b6a11dc6
AleksiewDev <aleksiew0832@gmail.com>
2025-11-03 13:10:36 +0200

    Update iajsd.js

--- iajsd.js
+++ iajsd.js
@@ -1076,6 +1076,18 @@ if (toggle && menu) {
     toggle.setAttribute('aria-expanded', String(isOpen));
   });
 }
+function setVh() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setVh();
+window.addEventListener('resize', setVh);
+
+
+
+
+
+
 
 
 
